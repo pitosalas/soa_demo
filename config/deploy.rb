cad9a32ff5f7a6e644a7b999e596ccfab02ceee6
Pito Salas <pitosalas@gmail.com>
2020-02-07 07:19:17 -0500

    tweaked cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock "~> 3.11.2"
 
 set :application, "soa_demo"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:pitosalas/soa_demo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/rails/soa_demo"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
