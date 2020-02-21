f8ca65475333a9f3210d387ff912f82b39a7473c
Pito Salas <pitosalas@gmail.com>
2020-02-21 17:34:37 +0000

    nothing

--- soa_publisher_node.rb
+++ soa_publisher_node.rb
@@ -9,8 +9,11 @@ class SoaPublisher
     @xface = TwitterApi.new
     @queue = TweetQueue.new
     @queue.establish
+    puts "about to connect"
     @xface.stream_connect
+    puts "connected"
     @xface.for_each_tweet do |t|
+      puts "."
       @queue.post(@xface.tweet_to_json(t))
     end
   end
--- twitter_api.rb
+++ twitter_api.rb
@@ -14,8 +14,6 @@ class TwitterApi
       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
       config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
       config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
-      `printenv |grep TWITTER`
-      puts "====="
     end
   end
 
