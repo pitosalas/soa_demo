41d2361ab194645d2a8a58b16d20b3ad6514ede0
Pito Salas <pitosalas@gmail.com>
2020-02-21 13:34:01 -0400

    Loggin

--- soa_publisher_node.rb
+++ soa_publisher_node.rb
@@ -1,17 +1,20 @@
 require 'twitter'
+require 'syslog/logger'
 require 'bunny'
 require_relative 'twitter_api'
 require_relative 'tweet_queue'
 
+
 class SoaPublisher
   def run
-    puts "*********************"
+    log = Syslog::Logger.new 'soa_publisher'
+    log.info 'Starting up'
     @xface = TwitterApi.new
     @queue = TweetQueue.new
     @queue.establish
     @xface.stream_connect
     @xface.for_each_tweet do |t|
-      puts "."
+      log.info '.'
       @queue.post(@xface.tweet_to_json(t))
     end
   end
