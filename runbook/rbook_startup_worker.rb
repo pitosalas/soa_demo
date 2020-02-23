75c82dfddfef44457097495ca4069a6966f64ef3
Pito Salas <pitosalas@gmail.com>
2020-02-23 15:54:59 -0400

    added server ip

--- runbook/rbook_startup_worker.rb
+++ runbook/rbook_startup_worker.rb
@@ -6,15 +6,19 @@ Runbook.book "Startup Worker" do
   server "rails@" + WORKER_SERVER_IP
   
   section "Run Primary" do
+    @my_env = {TWITTER_CONSUMER_KEY: TWTITER_CONSUMER_KEY,
+    PRIMARY_SERVER_IP: PRIMARY_SERVER_IP,
+    WORKER_SERVER_IP: WORKER_SERVER_IP,
+    RABBIT_SERVER_IP: RABBIT_SERVER_IP,
+    TWITTER_CONSUMER_SECRET: TWITTER_CONSUMER_SECRET,
+    TWITTER_ACCESS_TOKEN: TWITTER_ACCESS_TOKEN,
+    TWITTER_ACCESS_SECRET: TWITTER_ACCESS_SECRET}
     step "Start the soa_publisher node" do
-      env({TWITTER_CONSUMER_KEY: TWTITER_CONSUMER_KEY,
-           PRIMARY_SERVER_IP: PRIMARY_SERVER_IP,
-           WORKER_SERVER_IP: WORKER_SERVER_IP,
-           RABBIT_SERVER_IP: RABBIT_SERVER_IP,
-           TWITTER_CONSUMER_SECRET: TWITTER_CONSUMER_SECRET,
-           TWITTER_ACCESS_TOKEN: TWITTER_ACCESS_TOKEN,
-           TWITTER_ACCESS_SECRET: TWITTER_ACCESS_SECRET})
       server "rails@" + WORKER_SERVER_IP
+      ruby_command do
+        "pwd", raw: true, ssh_config: {env: @my_env}
+      end
+      command "printenv | grep ER", raw: true
       command "source ~/.rvm/scripts/rvm; ruby /home/rails/soa_demo/soa_publisher_node.rb", raw:true
     end
   end
